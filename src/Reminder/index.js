cd78968366166cb8a4802bc7c2b51b5c5283a6ec
fuser312 <sherltonGT40000>
2020-04-23 12:01:23 +0530

    Reminder app done.

--- src/Reminder/index.js
+++ src/Reminder/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './style.css'
 
 class Reminder extends React.Component {
     constructor(props) {
@@ -12,13 +13,39 @@ class Reminder extends React.Component {
 
 
     }
-    
+    componentWillMount() {
+        this.showReminderUntil(this.props.end);
+    }
 
+    componentDidMount() {
+        this.interval=setInterval(() => this.showReminderUntil(this.props.end), 1000);
+    }
+    componentWillUnmount() {
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
+    }
+
+    showReminderUntil(end) {
+        const time = Date.parse(end) - Date.parse(new Date());
+
+        if(time <0) {
+            this.props.deleteTimer();
+        } else {
+            const seconds = Math.floor((time/1000)%60);
+            const minutes = Math.floor((time/1000/60)%60);
+            const hours = Math.floor((time/(1000*60*60))%24);
+            const days = Math.floor(time/(1000*60*60*24));
+
+            this.setState({ days, hours, minutes, seconds });
+        }
+
+    }
 
     render() {
         return(
-            <div>
-                {this.props.date}
+            <div className="reminder">
+                {this.state.days} days {this.state.hours} hours {this.state.minutes} minutes {this.state.seconds} seconds
             </div>
         );
     }
---
+++ src/UserInput/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import './style.css'
+import Reminder from "../Reminder"
+
+class UserInput extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dateList: [],
+            timer: "",
+        }
+
+    }
+
+    getDate(timer) {
+        let tempList = this.state.dateList.slice();
+
+        tempList.push(timer);
+        this.setState({
+            timer: timer,
+            dateList: tempList,
+
+        })
+        console.log("just do it");
+
+        //let currentTime = new Date(Date.now());
+        //alert(currentTime);
+        //console.log(currentTime);
+        //     let finalList = [];
+        //     for(let i = 0; i< this.state.dateList.length; i++){
+        //         finalList.push(
+        //             <Reminder date = {this.state.dateList[i]} />
+        //                 )
+        //                 }
+        //     return finalList;
+         }
+
+        render()
+        {
+            let timer;
+            return (
+                <div>
+                <div className={"main"}>
+
+                    <div className={"datepicker"}>
+                        <label>Choose a day/time : </label>
+                        <input type="datetime-local" id={"time"} onChange={(time) => timer = time.target.value}/>
+                        <div className={"button"}>
+                            <button onClick={() => this.getDate(timer)}>Add Reminder</button>
+                        </div>
+                    </div>
+
+                </div>
+                    <div className="reminder">
+                        {this.state.dateList.map((item, index) => (<div>
+                            <Reminder end={this.state.dateList[index]} deleteTimer={() => {
+
+                                this.state.dateList.splice(index, 1);
+                                this.setState({
+                                    dateList: this.state.dateList
+                                });
+
+                            }}/>
+                        </div>))
+                        }
+                    </div>
+                </div>
+            )
+        }
+
+
+    }
+
+
+    export default UserInput;
